7baa010bf49125c75365c9ed32c4759cb2473c47
Bruce Hsu <1033684650@qq.com>
2024-01-12 14:12:17 +0800

    finish the login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,8 +3,21 @@
  * @Author: Bruce Hsu
  * @Description: 
  */
+
+// src/app/login/page.tsx
+
+// Custom Imports - Style
+import styles from "@/layouts/dashboard/login/login.module.css"
+
 export default function LoginPage() {
 	return (
-		<div>Login</div>
+		<div className={styles.container}>
+			<h2 className={styles.title}>Bruce Login Dashboard</h2>
+			<form action="" className={styles.form}>
+				<input type="text" name="username" placeholder="username"/>
+				<input type="text" name="password" placeholder="password"/>
+				<button>Login</button>
+			</form>
+		</div>
 	)
 }
