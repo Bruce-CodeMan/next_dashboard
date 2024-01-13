482f86e86de48ed5bb1b705919fe1cf45da948fa
Bruce Hsu <1033684650@qq.com>
2024-01-13 15:28:42 +0800

    add url search query

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -3,21 +3,36 @@
  * @Author: Bruce Hsu
  * @Description: 
  */
+"use client";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 import { MdSearch } from "react-icons/md"
 
 // Custom Imports - Style
 import styles from "./search.module.css"
 
+
 interface SearchProps {
 	placeholder: string;
 }
 
 export default function Search({placeholder}: SearchProps) {
+
+	const searchParams = useSearchParams();
+	const pathname = usePathname();
+	const { replace } = useRouter();
+
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const params = new URLSearchParams(searchParams)
+		params.set("q", e.target.value)
+		replace(`${pathname}?${params}`)
+	}
+
 	return (
 		<div className={styles.container}>
 			<MdSearch />
-			<input type="text" placeholder={placeholder} className={styles.input}/>
+			<input type="text" placeholder={placeholder} className={styles.input} onChange={handleChange}/>
 		</div>
 	)
 }
